a7e1352e95f5b349e3f8c38c1fb9ae312f375246
mayuino <mendo.woker@gmail.com>
2023-09-25 19:16:39 +0900

    Change Hello

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import Hello from "./src/components/Hello";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello style={{ fontSize: 16 }}>Small World</Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
@@ -15,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
